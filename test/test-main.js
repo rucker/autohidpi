7a4c34235394a8fc58797c5a0d7f2bba6b21cfa5
rucker <rick.ucker@gmail.com>
2015-07-01 19:28:07 -0400

    Unit tests for min width mode.

--- test/test-main.js
+++ test/test-main.js
@@ -1,7 +1,20 @@
-var main = require("./main");
+var main = require('./main');
+var simple_prefs = require('sdk/simple-prefs');
+var preferences = require('sdk/preferences/service');
 
-exports["test main"] = function(assert) {
-  assert.pass("Unit test running!");
+exports["test when user sets pixel ratio 1.2 " +
+        "and mode is min screen width " +
+        "and screen is greater than specified width " +
+        "then devPixelsPerPx is set to 1.2"] = function(assert) {
+  main.setDevPixelsPerPx(main.setMinWidthMode(5000, '1.2'));
+  assert.equal(preferences.get('layout.css.devPixelsPerPx'), '1.2');
+};
+
+exports["test when mode is min screen width " +
+        "and screen is not greater than specified width " +
+        "then devPixelsPerPx is set to 1"] = function(assert) {
+  main.setDevPixelsPerPx(main.setMinWidthMode(500, '1.2'));
+  assert.equal(preferences.get('layout.css.devPixelsPerPx'), '1');
 };
 
 exports["test main async"] = function(assert, done) {
