8de24a6ff1004749090259457c4a54ad747e5772
rucker <rick.ucker@gmail.com>
2015-07-01 19:58:04 -0400

    Unit tests for exact size mode.

--- test/test-main.js
+++ test/test-main.js
@@ -17,6 +17,21 @@ exports["test when mode is min screen width " +
   assert.equal(preferences.get('layout.css.devPixelsPerPx'), '1');
 };
 
+exports["test when user sets pixel ratio 1.2 " +
+        "and mode is exact screen size " +
+        "and screen matches specified screen size " +
+        "then devPixelsPerPx is set to 1.2"] = function(assert) {
+  main.setDevPixelsPerPx(main.setExactScreenSizeMode('1920', '1080', '1.2'));
+  assert.equal(preferences.get('layout.css.devPixelsPerPx'), '1.2');
+};
+
+exports["test when mode is exact screen size " +
+        "and screen does not match specified size " +
+        "then devPixelsPerPx is set to 1"] = function(assert) {
+  main.setDevPixelsPerPx(main.setExactScreenSizeMode('1920', '1200', '1.2'));
+  assert.equal(preferences.get('layout.css.devPixelsPerPx'), '1');
+};
+
 exports["test main async"] = function(assert, done) {
   assert.pass("async Unit test running!");
   done();
