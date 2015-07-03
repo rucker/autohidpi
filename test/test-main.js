351f23bf8671795710cf6aae44590fe2bf9d8850
rucker <rick.ucker@gmail.com>
2015-07-03 18:46:16 -0400

    Add/improve test scenarios to be more specific.

--- test/test-main.js
+++ test/test-main.js
@@ -2,34 +2,58 @@ var main = require('./main');
 var simple_prefs = require('sdk/simple-prefs');
 var preferences = require('sdk/preferences/service');
 
-exports["test when user sets pixel ratio 1.2 " +
-        "and mode is min screen width " +
+exports["test when mode is min screen width " +
+        "and user sets pixel ratio to 1.2 " +
+        "and devPixelsPerPx is set to -1 " +
+        "and screen is not greater than specified width " +
+        "then devPixelsPerPx is set to -1"] = function(assert) {
+  main.setDevPixelsPerPx(main.setMinWidthMode(1920, '-1',  '1.2'));
+  assert.equal(preferences.get('layout.css.devPixelsPerPx'), '-1');
+};
+
+exports["test when mode is min screen width " +
+        "and user sets pixel ratio to 1.2 " +
+        "and devPixelsPerPx is set to -1 " +
         "and screen is greater than specified width " +
         "then devPixelsPerPx is set to 1.2"] = function(assert) {
-  main.setDevPixelsPerPx(main.setMinWidthMode(5000, '1.2'));
+  main.setDevPixelsPerPx(main.setMinWidthMode(2880, '-1',  '1.2'));
   assert.equal(preferences.get('layout.css.devPixelsPerPx'), '1.2');
 };
 
 exports["test when mode is min screen width " +
+        "and user sets pixel ratio to 1.2 " +
+        "and devPixelsPerPx is set to 1.5 " +
         "and screen is not greater than specified width " +
-        "then devPixelsPerPx is set to 1"] = function(assert) {
-  main.setDevPixelsPerPx(main.setMinWidthMode(500, '1.2'));
-  assert.equal(preferences.get('layout.css.devPixelsPerPx'), '1');
+        "then devPixelsPerPx is set to -1"] = function(assert) {
+  main.setDevPixelsPerPx(main.setMinWidthMode(1920, '1.5',  '1.2'));
+  assert.equal(preferences.get('layout.css.devPixelsPerPx'), '-1');
 };
 
-exports["test when user sets pixel ratio 1.2 " +
-        "and mode is exact screen resolution " +
-        "and screen matches specified screen resolution " +
+exports["test when mode is exact screen resolution " + 
+        "and user sets pixel ratio 1.2 " +
+        "and devPixelsPerPx is set to -1 " +
+        "and screen matches specified resolution " +
         "then devPixelsPerPx is set to 1.2"] = function(assert) {
-  main.setDevPixelsPerPx(main.setExactScreenResolutionMode('1920', '1080', '1.2'));
+  main.setDevPixelsPerPx(main.setExactScreenResolutionMode('1920', '1080', '-1', '1.2'));
   assert.equal(preferences.get('layout.css.devPixelsPerPx'), '1.2');
 };
 
 exports["test when mode is exact screen resolution " +
+        "and user sets pixel ratio 1.2 " +
+        "and devPixelsPerPx is set to -1 " +
         "and screen does not match specified resolution " +
-        "then devPixelsPerPx is set to 1"] = function(assert) {
-  main.setDevPixelsPerPx(main.setExactScreenResolutionMode('1920', '1200', '1.2'));
-  assert.equal(preferences.get('layout.css.devPixelsPerPx'), '1');
+        "then devPixelsPerPx is set to -1"] = function(assert) {
+  main.setDevPixelsPerPx(main.setExactScreenResolutionMode('1920', '1200', '-1', '1.2'));
+  assert.equal(preferences.get('layout.css.devPixelsPerPx'), '-1');
+};
+
+exports["test when mode is exact screen resolution " + 
+        "and user sets pixel ratio 1.2 " +
+        "and devPixelsPerPx is set to 1.5 " +
+        "and screen matches specified resolution " +
+        "then devPixelsPerPx is set to 1.2"] = function(assert) {
+  main.setDevPixelsPerPx(main.setExactScreenResolutionMode('2880', '1620', '1.5', '1.2'));
+  assert.equal(preferences.get('layout.css.devPixelsPerPx'), '1.2');
 };
 
 require("sdk/test").run(exports);
